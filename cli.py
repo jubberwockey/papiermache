72fab64643a5814bdf2ce0e4160cafac29e4e553
Markus Krecik <markus.krecik@gmail.com>
2020-04-25 23:21:20 +0200

    adds timeout for cli

--- cli.py
+++ cli.py
@@ -1,21 +1,35 @@
 import sys
 from prompt_toolkit import PromptSession
 from utils.dispatcher import Dispatcher
+import asyncio
 
-s = PromptSession()
-d = Dispatcher(session=s)
 
-exit_cmds = d.get_exit_commands()
+def shutdown(dispatcher, msg=''):
+    print(msg)
+    dispatcher.shutdown()
+    sys.exit(0)
 
-while input not in exit_cmds:
+async def main():
+    s = PromptSession()
+    d = Dispatcher(session=s)
+
+    exit_cmds = d.get_exit_commands()
+    input = ''
+    while input not in exit_cmds:
+        try:
+            input = await asyncio.wait_for(s.prompt_async('>', completer=d.get_completer()), timeout=60)
+            d.execute(input)
+        except (KeyboardInterrupt, EOFError):
+            shutdown(d, "Manual Interrupt, exiting...")
+        except asyncio.TimeoutError:
+            shutdown(d, "Timeout, shutting down...")
+
+    shutdown(d, "Exiting...")
+
+
+if __name__ == '__main__':
     try:
-        input = s.prompt('>', completer=d.get_completer())
-        d.execute(input)
-    except (KeyboardInterrupt, EOFError):
-        print("Manual Interrupt, exiting...")
-        d.shutdown()
-        sys.exit()
-
-print("Exiting...")
-d.shutdown()
-sys.exit(0)
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        # need to clean up here
+        print("KeyboardInterrupt")
